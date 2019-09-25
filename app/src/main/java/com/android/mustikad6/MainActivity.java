14f1e0d607e13c8de3ef0a0ff2ee20b98095c631
Ach. Yasir Rofiqi <rofiqiachyasir@gmail.com>
2019-09-25 16:32:18 +0700

    Initial Commit

---
+++ app/src/main/java/com/android/mustikad6/MainActivity.java
@@ -0,0 +1,92 @@
+package com.android.mustikad6;
+
+import android.graphics.Bitmap;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.HttpAuthHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity{
+
+    SwipeRefreshLayout swipeRefreshLayout;
+    WebView webView;
+    WebSettings webSettings;
+    String URL = "https://mustikad6.000webhostapp.com/";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        swipeRefreshLayout = findViewById(R.id.refresh);
+        webView = findViewById(R.id.webView);
+
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.getUseWideViewPort();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+
+                if (null != swipeRefreshLayout){
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                swipeRefreshLayout.setRefreshing(true);
+            }
+
+        });
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else{
+            finish();
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()){
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
